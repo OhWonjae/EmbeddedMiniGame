46babaedcc6a2bd83ac8bdca1b8f865db5adfb8d
PC <PC@DESKTOP-CJVKRHR>
2026-01-31 10:41:45 +0900

    [feature/EXTI_버튼적용]
    버튼 누르는 동안 계속 감지하는 기능
    - EXTI FALLING, RASING TRIGGER 적용
    - EXIT 파일 분리 후 HAL_GPIO_EXTI_CALLBACK 함수사용 / Btn_Trigger 함수 분리로 HAL과
    어플리케이션 레벨 분리
    
    전역 변수 파일 적용
    - global 전역변수 파일 생성 및 인풋 값에대한 구조체 정의
    - .h파일에서 선언된 구조체를 extern, .c에서 초기화 (변수의 multiple definition 방지)
    
    메인루프 테스트
    - 전역 인풋 구조체 값을 계속 확인하여 버튼 누르는 동안 동작 하는지 테스트 완료

--- EmbeddedMiniGame/Core/Inc/global.h
+++ EmbeddedMiniGame/Core/Inc/global.h
@@ -1,7 +1,13 @@
-//#ifndef _GLOBAL_H
-//#define _GLOBAL_H
-//typedef Input{
-//
-//}
-//
-//#endif _GLOBAL_H
+#ifndef _GLOBAL_H
+#define _GLOBAL_H
+#include "stdint.h"
+/*
+ * .h파일에서 선언된 구조체를 extern, .c에서 초기화 (변수의 multiple definition 방지)
+ */
+typedef struct _INPUT{
+	uint8_t left_btn_down;
+	uint8_t right_btn_down;
+}Input;
+
+extern Input input;
+#endif _GLOBAL_H
---
+++ EmbeddedMiniGame/Core/Src/global.c
@@ -0,0 +1,5 @@
+#include "global.h"
+/*
+ * 전역변수 정의 파일
+ */
+Input input = {0,0};
